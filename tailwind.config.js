4e6d7f6f91e114bdc1c9c15f9528ec89efed8479
murzabek958-cloud <murzabek958@gmail.com>
2026-09-15 00:51:32 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        bronze: '#CD7F32',
+        gold: '#D4AF37',
+        darkbg: '#050507',
+      },
+      fontFamily: {
+        serif: ['"Playfair Display"', 'serif'],
+        sans: ['"Inter"', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
